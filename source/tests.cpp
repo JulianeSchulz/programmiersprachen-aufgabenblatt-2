2cf857303d406ebfbd1b70d07e228cd54c026257
JulianeSchulz <juliane.schulz@uni-weimar.de>
2016-05-04 15:59:10 +0200

    Tests fertig

--- source/tests.cpp
+++ source/tests.cpp
@@ -52,6 +52,50 @@ TEST_CASE("describe_divide", "[divide]")
 	REQUIRE(result.y == Approx(3.425f));
 }
 
+TEST_CASE("describe_additiontwo", "[additiontwo]")
+{
+	Vec2 addfirst{5.0f, 7.0f};
+	Vec2 addsecond{2.0f, 1.5f};
+	Vec2 result = addfirst + addsecond;
+	REQUIRE(result.x == Approx(7.0f));
+	REQUIRE(result.y == Approx(8.5f));
+}
+
+TEST_CASE("describe_subtractiontwo", "[subtractiontwo]")
+{
+	Vec2 one{6.0f, 15.0f};
+	Vec2 sub{2.7f, 4.0f};
+	Vec2 result = one - sub;
+	REQUIRE(result.x == Approx(3.3f));
+	REQUIRE(result.y == Approx(11.0f));
+}
+
+TEST_CASE("describe_multiplicatetwo", "[multiplicatetwo]")
+{
+	Vec2 v{8.6f, 2.4f};
+	float s = 6.0;
+	Vec2 result = v*s;
+	REQUIRE(result.x == Approx(51.6f));
+	REQUIRE(result.y == Approx(14.4f));
+}
+
+TEST_CASE("describe_dividetwo", "[dividetwo]")
+{
+	Vec2 v{40.0f, 27.4f};
+	float s = 8.0;
+	Vec2 result = v/s;
+	REQUIRE(result.x == Approx(5.0f));
+	REQUIRE(result.y == Approx(3.425f));
+}
+
+TEST_CASE("describe_multiplicatethree", "[multiplicatethree]")
+{
+	Vec2 v{8.6f, 2.4f};
+	float s = 6.0;
+	Vec2 result = s*v;
+	REQUIRE(result.x == Approx(51.6f));
+	REQUIRE(result.y == Approx(14.4f));
+}
 
 int main(int argc, char *argv[])
 {
