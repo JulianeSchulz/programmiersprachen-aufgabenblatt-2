b72696485e7e4491e6f6c66239b823cf5f6758b7
JulianeSchulz <juliane.schulz@uni-weimar.de>
2016-05-06 15:53:06 +0200

    Matrizen fertig

--- source/mat2.hpp
+++ source/mat2.hpp
@@ -5,21 +5,21 @@
 class Mat2
 {
 public:
-Mat2 ();
-Mat2 (float x11, float x21, float x12, float x22);
+	Mat2 ();
+	Mat2 (float x11, float x21, float x12, float x22);
 
-float x11;
-float x21;
-float x12;
-float x22;
+	float x11;
+	float x21;
+	float x12;
+	float x22;
 
-Mat2& operator*=(Mat2 const& m);
+	Mat2& operator*=(Mat2 const& m);
 
-float det()const{
-	float result;
-	result = (x11*x22)-(x21*x12);
-	return result;
-}
+	float det()const{
+		float result;
+		result = (x11*x22)-(x21*x12);
+		return result;
+	}
 
 };
 # endif // MAT2_HPP
